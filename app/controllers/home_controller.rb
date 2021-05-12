d41a65e630660f4f549c10f4bfacdcd6aba58365
Mouha <mouhamadoudioufsn@gmail.com>
2021-05-12 14:09:37 +0000

    Show product details on home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
   def index
-    
+    @produits = Produit.all
   end
 end
--- db/migrate/seeds.rb
+++ db/migrate/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Produit.create(nom: 'Iphone', description: 'Iphone 12 à vendre à prix abordable', catégorie: 'téléphones', prix: 450000, user_id: 1)
